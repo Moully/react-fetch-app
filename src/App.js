528e487c1f964faafc1d14a066e260836cca9c22
mstars_lab2_12 <lab_02_02@Lab-02-02s-iMac.local>
2022-03-25 17:14:19 +0800

    searchForm onsubmit

--- src/App.js
+++ src/App.js
@@ -14,26 +14,23 @@ class App extends React.Component {
 
 
 
-componentDidMount(){
-  axios.get("http://api.giphy.com/v1/gifs/trending?api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs")
-  .then(res => 
-    
-    
-    {
-      this.setState({
-        gifs: res.data.data
-      })
-      console.log(res);
-    })
-  .catch(error=>console.log("error fetching and parsing data", error))
-
-
+componentDidMount(){}
   // fetch("http://api.giphy.com/v1/gifs/trending?api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs")
   // .then(e=>e.json())
   // .then((data)=>{
   // this.setState({gifs: data.data})})
   // .catch(error=>{console.log("Error featching and parsing data", error);})
-}
+  performSearch = (query) =>{
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs`)
+    .then(res => 
+      {
+        this.setState({
+          gifs: res.data.data
+        })
+      })
+    .catch(error=>console.log("error fetching and parsing data", error))
+  }
+
 
 
   render(){
@@ -42,7 +39,7 @@ componentDidMount(){
         <div className='main-inner'>
           <h1 className='main-title'>Search App</h1>
         </div>
-        <Search  />
+        <Search onSearch={this.performSearch} />
         <div className="main-content">
           <GifList data={this.state.gifs}/>
         </div>
--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class Search extends React.Component {
-    
+    state = {
+        searchText: "",
+    }
+    onSearchChange = (e)=>{
+        this.setState({searchText: e.target.value})
+    }
+    handleSubmit = (e)=>{
+        console.log(e);
+        e.preventDefault()
+        e.currentTarget.reset()
+        this.props.onSearch(this.state.searchText)
+    }
     render(){
         return (
             <div>
-                <form className="search-form">
+                <form className="search-form" onSubmit={this.handleSubmit}>
                     <label className="is-hidden" htmlFor="search">Search</label>
-                    <input type="search" name="search" placeholder="Search..."></input>
+                    <input type="search" name="search" placeholder="Search..." onChange={this.onSearchChange}></input>
                     <button type="sumbit" id="submit" className="search-button">
                         <i className="material-icons icn-search"></i>
                     </button>
