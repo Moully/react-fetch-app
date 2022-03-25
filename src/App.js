88274f7df321a54e68e3625fd5691bed92b2427c
mstars_lab2_12 <lab_02_02@Lab-02-02s-iMac.local>
2022-03-25 16:09:58 +0800

    Axios ashiglah fetch bish

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Search from './components/searchForm';
 import GifList from './components/GifList';
-
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (){
@@ -12,13 +12,27 @@ class App extends React.Component {
     }
   }
 
+
+
 componentDidMount(){
-  fetch("http://api.giphy.com/v1/gifs/trending?api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs")
-  .then(e=>e.json())
-  .then((data)=>{
-  this.setState({gifs: data.data})})
-  .catch(error=>{console.log("Error featching and parsing data", error);})
-  
+  axios.get("http://api.giphy.com/v1/gifs/trending?api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs")
+  .then(res => 
+    
+    
+    {
+      this.setState({
+        gifs: res.data.data
+      })
+      console.log(res);
+    })
+  .catch(error=>console.log("error fetching and parsing data", error))
+
+
+  // fetch("http://api.giphy.com/v1/gifs/trending?api_key=M3rDkcVPTRTZFQNBzhIhOnkjQPpZICQs")
+  // .then(e=>e.json())
+  // .then((data)=>{
+  // this.setState({gifs: data.data})})
+  // .catch(error=>{console.log("Error featching and parsing data", error);})
 }
 
 
