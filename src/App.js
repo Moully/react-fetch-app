54b8a65ec9b69ff3271a8e1665eb54238fd77c5a
mstars_lab2_12 <lab_02_02@Lab-02-02s-iMac.local>
2022-03-25 14:10:36 +0800

    style upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="main-header">
+        <div className='main-inner'>
+          <h1 className='main-title'>Search App</h1>
+        </div>
 
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
